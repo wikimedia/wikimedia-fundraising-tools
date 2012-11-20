0015c9dd910db086a126131090cbcd22fdef64bc
Matthew Walker <mwalker@wikimedia.org>
2012-11-20 09:53:03 -0800

    Initial Commit of Amazon Audit Script
    Script to connect to AWS and reconcile those reports
    with what we see in Civi.
    
    Change-Id: I5303d74c632d78a099ddb2aece2b56edd1a03aee

---
+++ AmazonAudit/amazon-audit.py
@@ -0,0 +1,151 @@
+#!/usr/bin/python
+
+from amazon import Amazon
+import stomp
+from ConfigParser import SafeConfigParser
+from optparse import OptionParser
+from datetime import datetime
+import dateutil.parser
+import time
+import MySQLdb as MySQL
+import json
+import sys
+
+def main():
+    # === Extract options ===
+    parser = OptionParser(usage="usage: %prog [options] <# of seconds to audit>")
+    parser.add_option("-c", "--config", dest='configFile', default=None, help='Path to configuration file')
+    (options, args) = parser.parse_args()
+
+    if len(args) != 1:
+        parser.print_usage()
+        exit()
+        
+    startTime = datetime.fromtimestamp(int(time.time()) - int(args[0]))
+    print("Starting AWS audit from %s" % startTime.isoformat())
+
+    # === Get the configuration options ===
+    config = SafeConfigParser()
+    fileList = ['./amazon-config.cfg']
+    if options.configFile is not None:
+        fileList.append(options.configFile)
+    config.read(fileList)
+
+    # === Open up ze STOMP ===
+    host_and_ports = (config.get('Stomp', 'server'), config.getint('Stomp', 'port'))
+    sc = stomp.Connection(host_and_ports=[host_and_ports])
+    sc.start()
+    sc.connect()
+    
+    # === Connection to Amazon ===
+    aws = Amazon(
+        awsEndpoint = config.get('AwsConfig', 'endpoint'),
+        awsAccessKey = config.get('AwsConfig', 'accessKey'),
+        awsSecret = config.get('AwsConfig', 'secretKey')
+    )
+
+    # === Connection to MySQL ===
+    dbcon = MySQL.connect(
+        config.get('MySQL', 'host'),
+        config.get('MySQL', 'user'),
+        config.get('MySQL', 'password'),
+        config.get('MySQL', 'schema')
+    )
+    
+    # === Main Application ===
+    # --- Obtain AWS information ---
+    print("Obtaining AWS transactions for the period")
+    awsTransactions = aws.getAccountActivity( startTime, fpsOperation='Pay', status='Success' )
+    print("Obtained %d transactions" % len(awsTransactions))
+    
+    # --- Main loop: checks each aws transaction against the Civi database; adding it if it doesn't exist ---
+    injectCount = 0
+    for txn in awsTransactions:
+        # --- Query Civi DB
+        cur = dbcon.cursor()
+        cur.execute("SELECT trxn_id FROM civicrm_contribution WHERE trxn_id LIKE 'AMAZON %s%%';" % txn['TransactionId'])
+        rc = cur.rowcount
+        cur.close()
+        if rc >= 1:
+            print("Amazon txnid %s was found more than once in Civi! Ignoring this txnid." % txn['TransactionId'])
+            continue
+
+        # --- If we're here there's no entry in Civi. However the getAccountActivity call does not provide all the info
+        #       we need, so make another one, txn specific.
+        txnInfo = aws.getTransaction( txn['TransactionId'] )
+
+        # --- Confirm that the transaction has in fact completed before constructing the STOMP message
+        if txnInfo['StatusCode'] != 'Success':
+            sys.stderr.write("Amazon changed the status of %s on us!" % txn['TransactionId'])
+            continue
+
+        # Construct the STOMP message
+        headers = {
+            'correlation-id': 'amazon-%s' % txnInfo['CallerReference'],
+            'destination': config.get('Stomp', 'verified-queue'),
+            'persistent': 'true'
+        }
+        msg = {
+            "contribution_tracking_id": '',
+            "gateway_txn_id": txn['TransactionId'],
+
+            "email": txnInfo['SenderEmail'],
+            "first_name": txnInfo['SenderName'].split(' ')[0],
+            "middle_name":"",
+            "last_name": " ".join(txnInfo['SenderName'].split(' ')[1:]),
+            "last_name_2":"",
+
+            "currency": txnInfo['TransactionAmount']['CurrencyCode'],
+            "original_currency": txnInfo['TransactionAmount']['CurrencyCode'],
+            "original_gross": txnInfo['TransactionAmount']['Value'],
+            "fee":"0",
+            "gross": txnInfo['TransactionAmount']['Value'],
+            "net": txnInfo['TransactionAmount']['Value'],
+
+            "date": dateutil.parser.parse(txnInfo['DateReceived']).astimezone(dateutil.tz.tzutc()).strftime('%s'),
+
+            "gateway":"amazon",
+            "gateway_account": config.get('AwsConfig', 'accountName'),
+            "payment_method":"amazon",
+            "payment_submethod": txn['PaymentMethod'],
+            "referrer":"",
+            "comment":"", "size":"",
+            "premium_language":"en",
+            "language":"en",
+            "utm_source":"..amazon",
+            "utm_medium":"",
+            "utm_campaign":"",
+            "street_address":"",
+            "supplemental_address_1":"",
+            "city":"",
+            "state_province":"",
+            "country":"",
+            "street_address_2":"",
+            "supplemental_address_2":"",
+            "city_2":"",
+            "state_province_2":"",
+            "country_2":"",
+            "postal_code_2":"",
+            "user_ip":"",
+            "response":False,
+            "recurring":""
+        }
+
+        # Inject the message
+        print("Injecting found message into queue. Transaction ID: %s, Contribution ID: %s" % (txn['TransactionId'], txnInfo['CallerReference']))
+        sc.send(
+            json.dumps(msg),
+            headers
+        )
+        injectCount += 1
+
+    print("Of %d messages, %d were not found and were injected." % (len(awsTransactions), injectCount))
+
+    # === Final Application Cleanup ===
+    print("Finished. Cleaning up.")
+    dbcon.close()
+    sc.disconnect()
+    time.sleep(1)   # Let the STOMP library catch up
+
+if __name__ == "__main__":
+    main()
---
+++ AmazonAudit/amazon.py
@@ -0,0 +1,268 @@
+#!/usr/bin/python
+
+import urllib
+from urlparse import urlparse, parse_qsl, ParseResult, urlunparse
+from hashlib import sha256
+import hmac
+from base64 import b64encode
+from datetime import datetime
+from xml.dom.minidom import parse
+import dateutil.parser
+
+class Amazon:
+    """Has the following attributes:
+        self.awsEndpoint
+        self.awsAccessKey
+        self.awsSecret
+    """
+
+    AWS_FPS_VERSION_1 = '2010-08-28'
+
+    FPSOperation = [
+        'Pay',
+        'Refund',
+        'Settle',
+        'SettleDebt',
+        'WriteOffDebt',
+        'FundPrepaid',
+        'Reserve',
+    ]
+
+    TransactionStatus = [
+        'Cancelled',
+        'Failure',
+        'Pending',
+        'Reserved',
+        'Success'
+    ]
+
+    def __init__(self, awsEndpoint, awsAccessKey, awsSecret):
+        self.awsEndpoint = awsEndpoint
+        self.awsAccessKey = awsAccessKey
+        self.awsSecret = awsSecret
+
+    def doTransaction(self, action, version, params):
+        """Perform the requested AWS action.
+
+         -- Parameters
+         action  - AWS action name
+         version - API version string implemented
+         params  - Dictionary of parameters.
+
+         -- Returns
+         A DOM tree object. May raise an exception if AWS returns error nodes
+        """
+        params.update({
+            'Action': action,
+            'Version': version,
+        })
+        reqParams = self.signRequest('POST', params)
+        req = urllib.urlopen(self.awsEndpoint, urllib.urlencode(reqParams))
+        dom = parse(req)
+        errors = dom.getElementsByTagName("Error")
+        if len(errors) != 0:
+            errInfo = []
+            for error in errors:
+                errInfo.append(
+                    error.getElementsByTagName("Code")[0].firstChild.nodeValue + ': ' +
+                    error.getElementsByTagName("Message")[0].firstChild.nodeValue
+                )
+
+            errStr = errInfo[0]
+            if len(errInfo) > 1:
+                errStr += " also %s" % (", ".join(errStr[1:]))
+            raise AmazonException(errStr)
+
+        return dom
+
+    def signRequest(self, httpMethod, requestParams):
+        """ Sign an amazon request per http://docs.amazonwebservices.com/AmazonFPS/latest/FPSAccountManagementGuide/APPNDX_GeneratingaSignature.html
+         httpMethod - GET/POST
+         requestUri - Endpoint that will be accepting the signature
+         requestParams - Dictionary of parameters
+         secretKey - AWS secret key
+
+         returns - Signed URL if method is GET, otherwise list of parameters
+        """
+
+        if httpMethod not in ['GET','POST']:
+            raise Exception('Invalid HTTP Method')
+
+        # Process the requestURI
+        requestUriParts = urlparse(self.awsEndpoint)
+
+        # 1a) Take the inbound dict and sort it; combine with requestUriParts as required
+        paramList = requestParams.items()
+        if httpMethod is 'GET':
+            paramList += parse_qsl(requestUriParts.query)
+
+        # Add other signature parts
+        paramList += [
+            ('AWSAccessKeyId', self.awsAccessKey),
+            ('SignatureVersion', '2'),
+            ('SignatureMethod', 'HmacSHA256'),
+            ('Timestamp', datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'))
+        ]
+        paramList.sort()
+
+        # 1b) RFC 3986 Encode
+        # 1c) Create parameters by = separation
+        # 1d) Separate params by &
+        reqString = []
+        if len(paramList) > 0:
+            for k,v in paramList:
+                reqString.append("%s=%s" % (urllib.quote(str(k), ''), urllib.quote(str(v), '')))
+            reqString = "&".join(reqString)
+        else:
+            reqString = ''
+
+        # 2) Create the rest of the signing string
+        signString = "%s\n%s\n%s\n%s" % (
+            httpMethod,
+            requestUriParts.netloc,
+            requestUriParts.path or '/',
+            reqString
+        )
+
+        # 3) HMAC sign with SHA256
+        # 4) Format as base64
+        h = hmac.new(self.awsSecret, signString, sha256)
+        sig = b64encode(h.digest())
+        paramList += [('Signature', sig)]
+
+        # 5) This then becomes Signature=
+        if httpMethod is 'GET':
+            query = urlunparse(ParseResult(
+                requestUriParts.scheme,
+                requestUriParts.netloc,
+                requestUriParts.path,
+                '',
+                reduce(lambda x,y: '%s&%s=%s' % (x, str(y[0]), urllib.quote(str(y[1]), '')), paramList, '')[1:],
+                ''
+            ))
+        else:
+            query = paramList
+
+        return query
+
+    def getTransaction(self, transactionId):
+        """Get detailed information on an Amazon Payments transaction.
+
+        -- Parameters
+        transactionId - the transaction ID given by amazon that uniquely identifies it
+
+        -- Returns
+        Dictionary of values
+        """
+        params = {
+            'TransactionId':transactionId
+        }
+        dom = self.doTransaction( 'GetTransaction', Amazon.AWS_FPS_VERSION_1, params)
+
+        return self.treeToDict(dom.getElementsByTagName("Transaction")[0])
+
+    def getAccountActivity(self, startDate, endDate = None, fpsOperation = None, status = None, returnNext = False, limit = 0):
+        """Obtains all account activity matching the given filters.
+
+         -- Parameters
+         startDate      - Datetime or string to obtain transactions before
+         endDate        - Datetime or string to obtain transactions after
+         fpsOperation   - Must be one of Amazon.FPSOperation
+         status         - Must be one of Amazon.TransactionStatus
+         returnNext     - If True, the function will not continuously call the API. Instead it will return a tuple
+                            where the first element is next state date to retrieve from AWS. The second element are
+                            the retrieved transaction objects
+         limit          - Lower bound is 20 (or 0 for no bound)
+
+         -- Returns
+         A list of returned transactions. (Unless returnNext is True)
+        """
+        if not isinstance(startDate, datetime):
+            startDate = dateutil.parser.parse(startDate)
+        if (endDate is not None) and (type(endDate) != datetime):
+            endDate = dateutil.parser.parse(endDate)
+
+        params = {
+            'StartDate': startDate.isoformat(),
+            'SortOrderByDate': 'Ascending',
+        }
+        if endDate is not None:
+            params['EndDate'] = endDate.isoformat()
+        if fpsOperation is not None:
+            if fpsOperation in Amazon.FPSOperation:
+                params['FPSOperation'] = fpsOperation
+            else:
+                raise AmazonException('Provided fpsOperation (%s) argument not valid.' % fpsOperation)
+        if status is not None:
+            if status in Amazon.TransactionStatus:
+                params['TransactionStatus'] = status
+            else:
+                raise AmazonException('Provided status (%s) argument not valid.' % status)
+        if (limit != 0) and (limit < 200):
+            # AmazonAWS imposes a minimum batch size limit of 20 or it will throw an error
+            params['MaxBatchSize'] = max(20, limit)
+        elif limit < 0:
+            raise AmazonException('Cannot have a negative limit argument')
+        else:
+            params['MaxBatchSize'] = 200
+
+        dom = self.doTransaction('GetAccountActivity', Amazon.AWS_FPS_VERSION_1, params)
+        dom = self.treeToDict(dom.getElementsByTagName("GetAccountActivityResult")[0])
+
+        # Little bit of cleanup
+        if 'StartTimeForNextTransaction' in dom:
+            nextStart = dom['StartTimeForNextTransaction']
+        else:
+            nextStart = None
+
+        if 'Transaction' not in dom:
+            dom['Transaction'] = []
+
+        if returnNext:
+            # This behaviour allows us to chain things together to get everything
+            return nextStart, dom['Transaction']
+        else:
+            # Chain the requests until we get a none
+            transactions = dom['Transaction']
+
+            while (nextStart is not None) and ((limit == 0) or (len(transactions) < limit)):
+                if limit != 0:
+                    sublimit = max(20, limit - len(transactions))
+                else:
+                    sublimit = 200
+
+                (nextStart, tlist) = self.getAccountActivity(nextStart, endDate, fpsOperation, status, True, sublimit)
+                transactions += tlist
+                print("%s - %s" % (nextStart, len(transactions)))
+
+        return transactions
+
+    def treeToDict(self, domTree):
+        """Convert a DOM tree to something a little more useful. Assumes that tree elements have no attributes
+
+        -- Parameters
+        domTree - the DOMNode object to turn into a dictionary
+        """
+        tree = {}
+
+        node = domTree.firstChild
+        while node is not None:
+            if (node.firstChild is not None) and (node.firstChild.nodeType == node.TEXT_NODE):
+                tree[node.localName] = node.firstChild.nodeValue
+            else:
+                ntree = self.treeToDict(node)
+                if node.localName in tree:
+                    if hasattr(tree[node.localName], 'append'):
+                        tree[node.localName].append(ntree)
+                    else:
+                        tree[node.localName] = [
+                            tree[node.localName],
+                            ntree
+                        ]
+                else:
+                    tree[node.localName] = ntree
+            node = node.nextSibling
+        return tree
+
+class AmazonException(Exception):
+    pass
