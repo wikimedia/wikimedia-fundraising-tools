a2c5bce9dc2a60beaa0c83896b9b60600283c86c
Matthew Walker <mwalker@wikimedia.org>
2012-12-07 14:53:59 -0800

    Amazon Gets a Brain!
    And by that I mean we now have a history and a log file that can
    be used to speed up processing by looking back at what's already
    been downloaded and processed.
    
    Change-Id: I2dab118a1f9c1564898cf829a5679fe7e856aae5

--- AmazonAudit/amazon-audit.py
+++ AmazonAudit/amazon-audit.py
@@ -10,12 +10,17 @@ import time
 import MySQLdb as MySQL
 import json
 import sys
+import os
+
+AWS_HISTORY_FILE_VERSTR = "AWSHistory.1"
 
 def main():
     # === Extract options ===
     parser = OptionParser(usage="usage: %prog [options] <# of seconds to audit>")
     parser.add_option("-c", "--config", dest='configFile', default=None, help='Path to configuration file')
     parser.add_option("-g", "--gracePeriod", dest='gracePeriod', default=0, help='Number of seconds from now backwards to ignore')
+    parser.add_option("-i", "--historyFile", dest='historyFile', default=None, help='Stores any pending transactions and the last run time')
+    parser.add_option('-l', "--logFile", dest='logFile', default=None, help='Saves a log of all Amazon transactions')
     (options, args) = parser.parse_args()
 
     if len(args) != 1:
@@ -23,7 +28,7 @@ def main():
         exit()
         
     startTime = datetime.fromtimestamp(int(time.time()) - int(args[0]))
-    endTime = datetime.fromtimestamp(int(time.time()) - options.gracePeriod)
+    endTime = datetime.fromtimestamp(int(time.time()) - int(options.gracePeriod))
     print("Starting AWS audit from %s to %s" % (startTime.isoformat(), endTime.isoformat()))
 
     # === Get the configuration options ===
@@ -53,107 +58,258 @@ def main():
         config.get('MySQL', 'password'),
         config.get('MySQL', 'schema')
     )
+
+    # === Open up the history and log files ===
+    # If the history file exists, it will modify the start time of this script to be the end time of the
+    # history file.
+    hfile = None
+    historyStart = startTime
+    historyEnd = endTime
+    if options.historyFile and os.path.exists(options.historyFile):
+        hfile = open(options.historyFile, 'r')
+        if hfile.readline().strip() == AWS_HISTORY_FILE_VERSTR:
+            historyStart = dateutil.parser.parse(hfile.readline().strip())
+            historyEnd = dateutil.parser.parse(hfile.readline().strip())
+            startTime = historyEnd
+    else:
+        print('Not starting with a valid history file.')
+
+    sfile = None
+    if options.logFile:
+        sfile = open(options.logFile, 'a')
+        sfile.write("!!! Starting run for dates %s -> %s\n" % (startTime.isoformat(), endTime.isoformat()))
+
+    # === Sanity checks ===
+    if endTime > startTime:
+        endTime = startTime
     
     # === Main Application ===
-    # --- Obtain AWS information ---
-    print("Obtaining AWS transactions for the period")
-    awsTransactions = aws.getAccountActivity( startTime, endDate=endTime, fpsOperation='Pay', status='Success' )
+    # --- Process all previously pending transactions from the history file. If the transaction is still in some form
+    #     of pending, add it back to the history list.
+    historyCount = 0
+    historyList = []
+    historyStats = {
+        'Success': 0,
+        'Pending': 0,
+        'Failed': 0,
+        'Ignored': 0
+    }
+    if hfile:
+        print("Processing history file")
+        for txn in hfile:
+            historyCount += 1
+            txn = json.loads(txn)
+            result = processTransaction(txn, dbcon, aws, sc, sfile, config)
+            historyStats[result] += 1
+            if result == 'Pending':
+                historyList.append(txn)
+        hfile.close()
+
+    # --- Prepare the history file for write ---
+    if options.historyFile:
+        print("Rewriting history file with %d transactions" % len(historyList))
+        hfile = open(options.historyFile, 'w')
+        hfile.write("%s\n%s\n%s\n" % (AWS_HISTORY_FILE_VERSTR, historyStart.isoformat(), endTime.isoformat()))
+        for txn in historyList:
+            hfile.write("%s\n" % json.dumps(txn))
+        print("Flushing history file in preparation for main loop")
+        hfile.flush()
+
+    # --- Obtain AWS history ---
+    print("Obtaining AWS transactions for the period %s -> %s" % (startTime.isoformat(), endTime.isoformat()))
+    awsTransactions = aws.getAccountActivity(startTime, endDate=endTime, fpsOperation='Pay')
     print("Obtained %d transactions" % len(awsTransactions))
     
     # --- Main loop: checks each aws transaction against the Civi database; adding it if it doesn't exist ---
-    injectCount = 0
+    txncount = 0
+    notInCiviCount = 0
     for txn in awsTransactions:
-        # --- Query Civi DB
-        cur = dbcon.cursor()
-        cur.execute("SELECT trxn_id FROM civicrm_contribution WHERE trxn_id LIKE 'AMAZON %s%%';" % txn['TransactionId'])
-        rc = cur.rowcount
-        cur.close()
-        if rc >= 1:
-            print("Amazon txnid %s was found more than once in Civi! Ignoring this txnid." % txn['TransactionId'])
-            continue
+        txncount += 1
+        if not isTxnInCivi(txn['TransactionId'], dbcon):
+            notInCiviCount += 1
+            result = processTransaction(txn, dbcon, aws, sc, sfile, config)
+            historyStats[result] += 1
+            if result == 'Pending':
+                hfile.write("%s\n" % json.dumps(txn))
+                hfile.flush()
 
-        # --- If we're here there's no entry in Civi. However the getAccountActivity call does not provide all the info
-        #       we need, so make another one, txn specific.
-        txnInfo = aws.getTransaction( txn['TransactionId'] )
+    print("\n--- Finished processing of messages. ---\n")
 
-        # --- Get our contribution tracking ID (but it's stupid because AWS is stupid... ugh!)
-        ctid = None
-        for part in txn['TransactionPart']:
-            if part['Role'] == 'Recipient':
-                ctid = part['Reference']
-
-        # --- Confirm that the transaction has in fact completed before constructing the STOMP message
-        if txnInfo['StatusCode'] != 'Success':
-            sys.stderr.write("Amazon changed the status of %s on us!" % txn['TransactionId'])
-            continue
-
-        # Construct the STOMP message
-        headers = {
-            'correlation-id': 'amazon-%s' % txn['TransactionId'],
-            'destination': config.get('Stomp', 'verified-queue'),
-            'persistent': 'true'
-        }
-        msg = {
-            "contribution_tracking_id": ctid if ctid is not None else '',
-            "gateway_txn_id": txn['TransactionId'],
-
-            "email": txnInfo['SenderEmail'],
-            "first_name": txnInfo['SenderName'].split(' ')[0],
-            "middle_name":"",
-            "last_name": " ".join(txnInfo['SenderName'].split(' ')[1:]),
-            "last_name_2":"",
-
-            "currency": txnInfo['TransactionAmount']['CurrencyCode'],
-            "original_currency": txnInfo['TransactionAmount']['CurrencyCode'],
-            "original_gross": txnInfo['TransactionAmount']['Value'],
-            "fee":"0",
-            "gross": txnInfo['TransactionAmount']['Value'],
-            "net": txnInfo['TransactionAmount']['Value'],
-
-            "date": dateutil.parser.parse(txnInfo['DateReceived']).astimezone(dateutil.tz.tzutc()).strftime('%s'),
-
-            "gateway":"amazon",
-            "gateway_account": config.get('AwsConfig', 'accountName'),
-            "payment_method":"amazon",
-            "payment_submethod": txn['PaymentMethod'],
-            "referrer":"",
-            "comment":"", "size":"",
-            "premium_language":"en",
-            "language":"en",
-            "utm_source":"..amazon",
-            "utm_medium":"",
-            "utm_campaign":"",
-            "street_address":"",
-            "supplemental_address_1":"",
-            "city":"",
-            "state_province":"",
-            "country":"",
-            "street_address_2":"",
-            "supplemental_address_2":"",
-            "city_2":"",
-            "state_province_2":"",
-            "country_2":"",
-            "postal_code_2":"",
-            "user_ip":"",
-            "response":False,
-            "recurring":""
-        }
-
-        # Inject the message
-        print("Injecting found message into queue. Transaction ID: %s, Contribution ID: %s" % (txn['TransactionId'], ctid))
-        sc.send(
-            json.dumps(msg),
-            headers
-        )
-        injectCount += 1
-
-    print("Of %d messages, %d were not found and were injected." % (len(awsTransactions), injectCount))
+    print("Of %d new AWS messages, %d were not found in Civi." % (txncount, notInCiviCount))
+    print(" Additionally %d messages were processed from history" % historyCount)
+    print("This resulted in the following:")
+    for entry in historyStats.items():
+        print(" %s Messages: %d" % entry)
 
     # === Final Application Cleanup ===
-    print("Finished. Cleaning up.")
+    print("\nCleaning up.")
     dbcon.close()
     sc.disconnect()
+
+    if hfile:
+        hfile.close()
+    if sfile:
+        sfile.close()
+
     time.sleep(1)   # Let the STOMP library catch up
+
+def processTransaction(txn, dbcon, aws, sc, sfile, config):
+    """Main message processing logic. Will determine if a message needs to be injected or not
+
+    txn -- The transaction from getAccountActivity()
+    aws -- The AWS connection object
+    sc -- The Stomp connection object
+    sfile -- The log file
+    config -- The configuration object
+
+    returns "Success on injection, Pending on AWS pending, Failed on AWS failure, Ignored on already present in Civi
+    """
+    retval = ''
+    smallString = '.'
+    ctid = '?'
+
+    if (txn['TransactionStatus'] != 'Failure') or (not isTxnInCivi(txn['TransactionId'], dbcon)):
+        # Get additional information about the transaction because getAccountActivity does not provide all
+        # the required information. We also have to check what the status of this transaction is.
+        txnInfo = aws.getTransaction( txn['TransactionId'] )
+
+        # Do that aforementioned status check
+        if txnInfo['TransactionStatus'] == 'Success':
+            ctid = remediateTransaction(txn, txnInfo, sc, config)
+            retval = 'Success'
+            smallString = '+'
+        elif txnInfo['TransactionStatus'] == 'Pending' or txnInfo['TransactionStatus'] == 'Reserved':
+            retval = 'Pending'
+            smallString = '-'
+        else:
+            retval = 'Failed'
+    else:
+        retval = 'Ignored'
+
+    bigString = "%s\t%s\t%s\t%s\t%s\t%s\t%s" % (
+        retval,
+        txn['TransactionStatus'],
+        txn['TransactionId'],
+        ctid if ctid is not None else '?',
+        dateutil.parser.parse(txn['DateReceived']).astimezone(dateutil.tz.tzutc()).isoformat(),
+        dateutil.parser.parse(txn['DateCompleted']).astimezone(dateutil.tz.tzutc()).isoformat(),
+        datetime.now(dateutil.tz.tzutc()).isoformat()
+    )
+
+    if sfile:
+        sfile.write("%s\n" % bigString)
+        sys.stdout.write(smallString)
+        sys.stdout.flush()
+    else:
+        print(bigString)
+
+    return retval
+
+def isTxnInCivi(txnid, dbcon):
+    """ Query the Civi database to determine if the txnid is present.
+
+    txnid -- The Amazon transaction ID
+    dbcon -- Database connection object
+
+    returns True if transaction is present in Civi
+    """
+
+    cur = dbcon.cursor()
+    cur.execute("SELECT trxn_id FROM civicrm_contribution WHERE trxn_id LIKE 'AMAZON %s%%';" % txnid)
+    rc = cur.rowcount
+    cur.close()
+    if rc >= 1:
+        return True
+    else:
+        return False
+
+def remediateTransaction(txn, txnInfo, sc, config):
+    """Injects a new message into the queue for consumption by Civi
+
+    txn -- The transaction data given from the getAccountActivity call
+    txnInfo -- Transaction data given from the getTransaction call
+    sc -- Stomp queue object
+    config -- Configuration object
+
+    returns -- The contribution tracking ID
+    """
+
+    # --- Get our contribution tracking ID (but it's stupid because AWS is stupid... ugh!)
+    ctid = None
+    if isinstance(txn['TransactionPart'], list):
+        for part in txn['TransactionPart']:
+            if part['Role'] == 'Recipient' and 'Reference' in part:
+                ctid = part['Reference']
+    elif txn['TransactionPart']['Role'] == 'Recipient' and 'Reference' in txn['TransactionPart']:
+        ctid = txn['TransactionPart']['Reference']
+
+    if ctid is not None:
+        try:
+            ctid = int(ctid)
+        except ValueError:
+            if not '-' in ctid:
+                # It's not a number or a UUID... very strange... not using is
+                ctid = None
+
+    # Construct the STOMP message
+    headers = {
+        'correlation-id': 'amazon-%s' % txn['TransactionId'],
+        'destination': config.get('Stomp', 'verified-queue'),
+        'persistent': 'true'
+    }
+    msg = {
+        "contribution_tracking_id": ctid if ctid is not None else '',
+        "gateway_txn_id": txn['TransactionId'],
+
+        "email": txnInfo['SenderEmail'],
+        "first_name": txnInfo['SenderName'].split(' ')[0],
+        "middle_name":"",
+        "last_name": " ".join(txnInfo['SenderName'].split(' ')[1:]),
+        "last_name_2":"",
+
+        "currency": txnInfo['TransactionAmount']['CurrencyCode'],
+        "original_currency": txnInfo['TransactionAmount']['CurrencyCode'],
+        "original_gross": txnInfo['TransactionAmount']['Value'],
+        "fee":"0",
+        "gross": txnInfo['TransactionAmount']['Value'],
+        "net": txnInfo['TransactionAmount']['Value'],
+
+        "date": dateutil.parser.parse(txnInfo['DateReceived']).astimezone(dateutil.tz.tzutc()).strftime('%s'),
+
+        "gateway":"amazon",
+        "gateway_account": config.get('AwsConfig', 'accountName'),
+        "payment_method":"amazon",
+        "payment_submethod": txn['PaymentMethod'],
+        "referrer":"",
+        "comment":"", "size":"",
+        "premium_language":"en",
+        "language":"en",
+        "utm_source":"..amazon",
+        "utm_medium":"",
+        "utm_campaign":"",
+        "street_address":"",
+        "supplemental_address_1":"",
+        "city":"",
+        "state_province":"",
+        "country":"",
+        "street_address_2":"",
+        "supplemental_address_2":"",
+        "city_2":"",
+        "state_province_2":"",
+        "country_2":"",
+        "postal_code_2":"",
+        "user_ip":"",
+        "response":False,
+        "recurring":""
+    }
+
+    # Inject the message
+    sc.send(
+        json.dumps(msg),
+        headers
+    )
+
+    return ctid
 
 if __name__ == "__main__":
     main()
