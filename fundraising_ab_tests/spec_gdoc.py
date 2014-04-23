cc043a4f37f96a53a66c409cd2e27c67887fb002
Adam Roses Wight <awight@wikimedia.org>
2014-04-22 18:42:44 -0700

    resolve spec sheet issues
    Change-Id: I9dc9a3fba9b0383444351da64507a369524086f5

--- fundraising_ab_tests/spec_gdoc.py
+++ fundraising_ab_tests/spec_gdoc.py
@@ -3,7 +3,8 @@ from google.gdocs import Spreadsheet
 from process.logging import Logger as log
 
 def read_gdoc_spec(doc=None):
-    return FrTestSpec(spec=list(parse_spec(Spreadsheet(doc=doc).get_all_rows())))
+    rows = list(Spreadsheet(doc=doc).get_all_rows())
+    return FrTestSpec(spec=list(parse_spec(rows)))
 
 def update_gdoc_spec(doc=None, spec=None):
     log.info("Updating test specs with latest CentralNotice changes... {url}".format(url=doc))
@@ -16,16 +17,16 @@ def update_gdoc_spec(doc=None, spec=None):
 
     doc = Spreadsheet(doc=doc)
     for index, test in enumerate(spec.spec, 0):
-        test = spec.spec[index]
-        rownum = index + 1
-        if rownum < doc.num_rows():
+        api_rownum = index + 1
+        rownum = index + 2
+        if api_rownum < doc.num_rows():
             if not hasattr(test, 'modified') or not test.modified:
                 continue
             log.debug("updating spec end time in row {rownum}: {spec}".format(rownum=rownum, spec=test))
             if test.end_time:
-                doc.update_row({'end': test.end_time}, index=rownum)
+                doc.update_row({'end': test.end_time}, index=api_rownum)
         else:
-            log.debug("appending spec row {rownum}: {spec}".format(rownum=index, spec=test))
+            log.debug("appending spec row {rownum}: {spec}".format(rownum=rownum, spec=test))
             doc.append_row({
                 'label': test.label,
                 'type': "banner",
