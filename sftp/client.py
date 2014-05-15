260db77ac56486fa45210cd879c533db7d3f705a
Adam Roses Wight <awight@wikimedia.org>
2014-05-15 12:56:44 -0700

    Generalize the nightly download process
    Deployment:
    * Must follow config file changes
    
    Change-Id: Ieb9cabc16fe2a6b42a5ca76b4ffa799ad6bf1794

--- sftp/client.py
+++ sftp/client.py
@@ -1,26 +1,33 @@
 import os, os.path
 import base64
 import paramiko
+import StringIO
 
 from process.logging import Logger as log
 from process.globals import config
 
 class Client(object):
-    def __init__(self, host_rsa_key=None, host_dss_key=None):
-        if host_dss_key:
-            self.host_public_key = paramiko.DSSKey(data=base64.decodestring(host_dss_key))
-        elif host_rsa_key:
-            self.host_public_key = paramiko.RSAKey(data=base64.decodestring(host_rsa_key))
-
+    def __init__(self):
+        self.client = None
         self.connect()
 
     def __del__(self):
-        self.client.close()
+        if self.client:
+            self.client.close()
 
     def connect(self):
         log.info("Connecting to {host}".format(host=config.sftp.host))
         transport = paramiko.Transport((config.sftp.host, 22))
-        transport.connect(username=config.sftp.username, password=config.sftp.password, hostkey=self.host_public_key)
+        params = {
+            'username': config.sftp.username,
+        }
+        if hasattr(config.sftp, 'host_key'):
+            params['hostkey'] = make_key(config.sftp.host_key)
+        if hasattr(config.sftp, 'password'):
+            params['password'] = config.sftp.password
+        if hasattr(config.sftp, 'private_key'):
+            params['pkey'] = make_key(config.sftp.private_key)
+        transport.connect(**params)
         self.client = paramiko.SFTPClient.from_transport(transport)
 
     def ls(self, path):
@@ -34,3 +41,77 @@ class Client(object):
                 log.info("Removing corrupted download: {path}".format(path=dest_path))
                 os.unlink(dest_path)
             raise
+
+class Crawler(object):
+    @staticmethod
+    def pull():
+        '''Pull down new remote files'''
+
+        # Check against both unprocessed and processed files to find new remote files
+        local_paths = [
+            config.incoming_path,
+            config.archive_path,
+        ]
+        local_files = walk_files(local_paths)
+
+        remote = Client()
+        remote_files = remote.ls(config.sftp.remote_root)
+
+        for filename in remote_files:
+            if filename in local_files:
+                log.info("Skipping already downloaded file {filename}".format(filename=filename))
+                continue
+
+            log.info("Downloading file {filename}".format(filename=filename))
+            dest_path = os.path.join(config.incoming_path, filename)
+            remote.get(os.path.join(config.sftp.remote_root, filename), dest_path)
+
+            # Assert that the file is not empty
+            if os.path.getsize(dest_path) == 0:
+                os.unlink(dest_path)
+                # Defaults to PANIC
+                if hasattr(config, 'panic_on_empty') and not config.panic_on_empty:
+                    log.warn("Stupid file was empty, removing locally: {path}".format(path=dest_path))
+                raise RuntimeError("Stupid file did not download correctly.")
+
+def walk_files(paths):
+    '''List all files under these path(s)
+
+    Parameters
+    ==========
+    * paths - single or list of paths
+    
+    Return value
+    ============
+    A list of all files found under the root directory(ies)
+    '''
+
+    result = []
+
+    if not hasattr(paths, 'extend'):
+        paths = [paths]
+
+    for root in paths:
+        for dirpath, dirnames, filenames in os.walk(root):
+            result += filenames
+
+    return result
+
+def make_key(keystr=None):
+    '''Cheesily detect a key string's type and create a Key object from it
+
+    FIXME: janky pseudo-standard format in use: 'ssh-rsa <KEYMATERIAL>'
+    '''
+
+    if 'BEGIN RSA PRIVATE KEY' in keystr:
+        fileish = StringIO.StringIO(keystr)
+        return paramiko.RSAKey.from_private_key(fileish)
+    elif 'ssh-rsa' in keystr:
+        return paramiko.RSAKey(data=base64.decodestring(keystr.split(' ')[1]))
+    elif 'BEGIN DSS PRIVATE KEY' in keystr:
+        fileish = StringIO.StringIO(keystr)
+        return paramiko.DSSKey.from_private_key(fileish)
+    elif 'ssh-dss' in keystr:
+        return paramiko.DSSKey(data=base64.decodestring(keystr.split(' ')[1]))
+
+    raise Exception('Unknown key provided')
