88cb7ecde7019e798be39dbc3483aeaccf6a83a0
Adam Roses Wight <awight@wikimedia.org>
2014-05-19 16:36:09 -0700

    Failmail module
    Change-Id: I01fd608afef990f26c5ddda52100955249a865ab

---
+++ failmail/mailer.py
@@ -0,0 +1,37 @@
+from email.mime.text import MIMEText
+import smtplib
+import sys
+import traceback
+import yaml
+
+from process.globals import config
+from process.logging import Logger as log
+
+class FailMailer(object):
+    @staticmethod
+    def mail(errorcode, data=None, print_exception=False):
+        body = ""
+        if print_exception:
+            exception_info = "".join(traceback.format_exception(*sys.exc_info()))
+            body = body + exception_info
+        if data:
+            if not isinstance(data, basestring):
+                data = yaml.safe_dump([data], default_flow_style=False)
+            body = body + "\n\nWhile processing:\n{data}".format(data=data)
+
+        log.error("sending failmail: " + body)
+
+        msg = MIMEText(body)
+
+        from_address = config.failmail_sender
+        to_address = config.failmail_recipients
+        if hasattr(to_address, 'split'):
+            to_address = to_address.split(",")
+
+        msg['Subject'] = "Fail Mail: {code} ({process})".format(code=errorcode, process=config.app_name)
+        msg['From'] = from_address
+        msg['To'] = to_address[0]
+
+        mailer = smtplib.SMTP('localhost')
+        mailer.sendmail(from_address, to_address, msg.as_string())
+        mailer.quit()
