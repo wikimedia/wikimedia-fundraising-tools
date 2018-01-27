a0006fcd0dddf76325d89c7cea973982507eb77b
Adam Wight <awight@wikimedia.org>
2018-01-27 18:31:46 -0500

    Remove logging shim
    Change-Id: Idfbd10e96bdcc931d35c57cfd116d5d8d4b0344f

--- process/log.py
+++ process/log.py
@@ -1,9 +1,5 @@
 """
 Helper to make logging go to syslog and optionally stdout.
-
-TODO:
-* Deprecate Logger and use the usual pattern:
-  log = logging.getLogger(__name__)
 """
 import logging
 import logging.config
@@ -11,32 +7,6 @@ import logging.config
 import process.globals
 
 _is_setup = False
-# TODO: Modules should do this internally, so logging happens under the correct
-# package name and can be filtered.
-_log = logging.getLogger(__name__)
-
-
-# Deprecated.
-class Logger(object):
-    @staticmethod
-    def debug(message):
-        _log.debug(message)
-
-    @staticmethod
-    def info(message):
-        _log.info(message)
-
-    @staticmethod
-    def warn(message):
-        _log.warning(message)
-
-    @staticmethod
-    def error(message):
-        _log.error(message)
-
-    @staticmethod
-    def fatal(message):
-        _log.critical(message)
 
 
 def setup_logging():
