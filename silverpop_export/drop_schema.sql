39eb88f39cbe708b15005028cc1fe323f5124d27
eileen <emcnaughton@wikimedia.org>
2020-08-05 10:53:38 +1200

    Remove drop of no-longer relevant table
    Bug: T253152
    
    Change-Id: I9119c2c5801c27deb196cf796f8519b074e75ac2

--- silverpop_export/drop_schema.sql
+++ silverpop_export/drop_schema.sql
@@ -1,5 +1,4 @@
 # The goal is not to have to drop this each time in future....
-DROP TABLE IF EXISTS silverpop_export_dedupe_email;
 DROP TABLE IF EXISTS `silverpop_export_matching_gift`;
 DROP TABLE IF EXISTS silverpop_email_map;
 DROP TABLE IF EXISTS silverpop_missing_countries;
