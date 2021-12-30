4b309ca9bf6c9bfbee5a021d83560a39411b3098
Eileen McNaughton <emcnaughton@wikimedia.org>
2021-12-31 12:12:38 +1300

    Add 'begin'
    This is referred to in the comments but is missing, making the comments wrong
    
    Bug: T286934
    Change-Id: I9560dc5589798016e3c8b4ca3221553ab3ef024a

--- silverpop_export/update_silverpop_staging.sql
+++ silverpop_export/update_silverpop_staging.sql
@@ -28,7 +28,7 @@ CREATE TABLE silverpop_update_world (
 -- The email suppression list uses MAX(id) from this table as it's upper bound so we want
 -- the table to be in a consistent state up to that point in time. If something fails
 -- the whole commit fails.
-
+BEGIN;
 -- Create a table of countries and languages for contacts with no country
 -- pulling data from contribution tracking.
 -- low volume of 'new catches'
