ef82d1a03833fa5bbd337ee75e8eeb8c6a44579b
eileen <emcnaughton@wikimedia.org>
2024-01-30 16:40:55 +1300

    Fix transposed segment,status
    Bug: T353386
    Bug: T339296
    
    Change-Id: I17ef526cfe960ad80048e6cd96ab5c936f2e41b3

--- silverpop_export/update_table.sql
+++ silverpop_export/update_table.sql
@@ -421,7 +421,7 @@ SELECT ex.id, dedupe_table.modified_date, ex.contact_id,ex.contact_hash,ex.first
   COALESCE(lt.latest_native_amount, 0) as foundation_latest_native_amount,
   foundation_last_donation_date,foundation_first_donation_date,
   addr.city,addr.country,addr.state,addr.postal_code,
-  stats.donor_status_id, stats.donor_segment_id,
+  stats.donor_segment_id, stats.donor_status_id,
   endowment_last_donation_date, endowment_first_donation_date,
   endowment_number_donations,
   COALESCE(endowment_highest_usd_amount,0) as endowment_highest_usd_amount,
