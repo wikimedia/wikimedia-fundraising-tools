dbdc753fcdd8ac37e0703382f863fdcd42c48f43
eileen <emcnaughton@wikimedia.org>
2025-11-19 19:24:28 +1300

    Stop exporting id in the suppression file
    This requires https://gerrit.wikimedia.org/r/c/wikimedia/fundraising/crm/+/1207462
    to be deployed first.
    
    In conjunction with deploying this we would alter the process_control job
    per the phab
    
    Bug: T410479
    
    Change-Id: I5c581efdcbbcbfcc844a8604c1b5f958cb8e95bb

--- silverpop_export/update_suppression_list.sql
+++ silverpop_export/update_suppression_list.sql
@@ -84,4 +84,4 @@ FROM silverpop_excluded
   ON m.uf_name = silverpop_excluded.email;
 
 CREATE OR REPLACE VIEW silverpop_excluded_utf8 as
-  SELECT CONVERT(email USING utf8) as email, id FROM silverpop_excluded;
+  SELECT CONVERT(email USING utf8) as email FROM silverpop_excluded;
