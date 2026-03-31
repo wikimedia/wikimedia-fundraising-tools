71a7b5472b03f7e2d11690d85464e2b6018ae828
Jack Gleeson <jgleeson@wikimedia.org>
2026-03-31 19:10:19 +0100

    Add OpenSSH private key format support to SFTP client
    Bug: T421772
    Change-Id: I9b648d4f22b13f95d21c2cd6c1dd22e0a4dfdf2c

--- sftp/client.py
+++ sftp/client.py
@@ -137,5 +137,8 @@ def make_key(keystr=None):
         return paramiko.DSSKey(data=base64.b64decode(keystr.split(' ')[1]))
     elif 'ecdsa-' in keystr:
         return paramiko.ECDSAKey(data=base64.b64decode(keystr.split(' ')[1]))
+    elif 'BEGIN OPENSSH PRIVATE KEY' in keystr:
+        fileish = io.StringIO(keystr)
+        return paramiko.Ed25519Key.from_private_key(fileish)
 
     raise Exception('Unknown key provided')
